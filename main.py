aa8472c735398e56ebde4c72015185a79e4dd5b2
rt2003 <rt2003@HW-GRCO-PC013>
2023-11-16 11:45:00 +0000

    testing

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from read_data import Square
+
 
 if __name__ == '__main__':
-    newClass = Square(5)
-    val = newClass.get_val()
-
-    print(val)
+    
--- read_data.py
+++ read_data.py
@@ -1,6 +1,1 @@
-class Square:
-    def __init__(self, val):
-        self.val = val
 
-    def get_val(self):
-        return self.val * self.val
