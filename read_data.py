c5af15921a03dbce00ec8ca1e220e6290d9774f3
Hasan Mohammad <35733550+HMohammad1@users.noreply.github.com>
2023-11-16 14:20:14 +0000

    task 1 return count of each country tests

--- read_data.py
+++ read_data.py
@@ -8,11 +8,11 @@ class ReadData:
         global df
         df = pd.read_json('sample_small.json', lines=True)
         dataframe = pd.DataFrame(df, columns=['env_doc_id', 'visitor_country'])
-        rslt_df = dataframe[(dataframe['env_doc_id'] == '140224132818-2a89379e80cb7340d8504ad002fab76d')]
-        cool = len(dataframe[(dataframe['env_doc_id'] == '140224132818-2a89379e80cb7340d8504ad002fab76d') & (dataframe['visitor_country']== 'TH')])
+        rslt_df = dataframe[(dataframe['env_doc_id'] == '130603065734-ceaa7b2404edf4a85e5563d0860e9b65')]
+        cool = len(dataframe[(dataframe['env_doc_id'] == '140223031538-3e15e64ec39fbe82dabe39ac43dc4a63')])
         x = rslt_df['visitor_country'].value_counts()
         # print(dataframe.to_string())
-        print(x)
+        print(rslt_df.to_string())
 
     def query(self):
         df1 = df[['env_doc_id']]
