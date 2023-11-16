34340eb7f766ad5de71d6cc05ab0e762774766fe
Hasan Mohammad <35733550+HMohammad1@users.noreply.github.com>
2023-11-16 12:03:36 +0000

    tkinter example

---
+++ gui.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+
+class Gui:
+    def __init__(self):
+        master = Tk()
+        w = Canvas(master, width=400, height=600)
+        w.pack()
+        canvas_height = 20
+        canvas_width = 200
+        y = int(canvas_height / 2)
+        w.create_line(0, y, canvas_width, y)
+        mainloop()
