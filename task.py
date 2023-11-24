a559092590cb79dc94860c12c90566e102a74957
Rebekah Tong <rebekahxgtong@gmail.com>
2023-11-24 15:38:15 +0000

    added unique() and values() for x and y passes doc_id to read data

--- task.py
+++ task.py
@@ -1,35 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from read_data import ReadData
-import pandas as pd
 
 plt.style.use('fivethirtyeight') # makes the graph look nicer
 
 class Task:
-    def __init__(self):
-        print("hi")
 
+    global df
+
+    def __init__(self, doc_id):
+        data = ReadData() # do not delete - required for line below
+        self.df = data.get_df(doc_id) # gets dataframe
+
+    def unique(self):
+        unique = self.df['visitor_country'].unique() # find unique y-labels
+        unique = np.array(unique) # convert into array
+        return unique
+
+    def values(self):
+        values = self.df['visitor_country'].value_counts() # find number of occurences for each label
+        values = np.array(values) # convert into array
+        return values
+    
     def show(self):
-        data = ReadData() # do not delete. line required for code below.
-
-        countries_df = ReadData.countries_df() # data frame for countries
-
-        countries_unique = countries_df['visitor_country'].unique() # find unique countries
-        countries_unique = np.array(countries_unique) # convert into array
-
-        countries_values = countries_df['visitor_country'].value_counts() # find number of occurances for each country
-        countries_values = np.array(countries_values) # convert into array
-
-        plt.bar(countries_unique, countries_values, color='#444444', label='Viewers') # creates bar chart (len of args 0 and 1 must be equal)
-
-        plt.title('Histogram of Countries') # name of graph
-        plt.xlabel('Countries') # x axis label
-        plt.ylabel('Total Viewers') # y axis label
-
-        plt.tight_layout()
-        plt.legend()
-        plt.show()
-        
+        print("")
         #data = d.query()
         #xpoints = np.array([0, 6])
         #ypoints = np.array([0, 250])
