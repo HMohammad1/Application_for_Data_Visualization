fec8b614889aa524cc9cd0e6f3d9ea9c64ed4483
Hasan Mohammad <35733550+HMohammad1@users.noreply.github.com>
2023-11-29 19:02:28 +0000

    Add files via upload

--- read_data.py
+++ read_data.py
@@ -5,7 +5,7 @@ class ReadData:
     global data
 
     def __init__(self):
-        self.data = pd.read_json('sample_small.json', lines=True)
+        self.data = pd.read_json('sample_100k_lines.json', lines=True)
 
     def get_country_df(self, doc_id):
         dataframe = pd.DataFrame(self.data, columns=['env_doc_id', 'visitor_country'])
@@ -16,3 +16,13 @@ class ReadData:
         dataframe = pd.DataFrame(self.data, columns=['visitor_uuid', 'event_readtime'])
         rslt_df = dataframe[pd.notna(dataframe['event_readtime'])]
         return rslt_df
+
+    def get_visitor_df(self, doc_id):
+        # return all viewer ID's of the doc ID given
+        return self.data.loc[self.data['env_doc_id'] == doc_id, 'visitor_uuid'].astype(str)
+
+    def get_visitor_doc_id(self, visitor):
+        # return all the doc ID's for the given viewer but get rid of duplicates
+        doc = self.data[self.data['visitor_uuid'] == visitor]['env_doc_id'].astype(str)
+        return set(doc.unique())
+
